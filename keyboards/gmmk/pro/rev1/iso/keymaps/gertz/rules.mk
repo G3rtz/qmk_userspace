1dfa4165d0f897556973c7815b3fb9b52e052d62
G3rtz <algertz@proton.me>
2024-10-06 21:49:03 +0200

    Update rules.mk

--- keyboards/gmmk/pro/rev1/iso/keymaps/gertz/rules.mk
+++ keyboards/gmmk/pro/rev1/iso/keymaps/gertz/rules.mk
@@ -1,1 +1,7 @@
-ENCODER_MAP_ENABLE = yes
+LTO_ENABLE = yes               # link time optimization -- achieves a smaller compiled size
+BOOTMAGIC_ENABLE = yes         # Enable Bootmagic Lite
+VIA_ENABLE = yes\
+
+IDLE_TIMEOUT_ENABLE = yes
+STARTUP_NUMLOCK_ON = yes
+
